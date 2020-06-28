27ac75a2313ddbd0e0560a2e519b264abed8593b
Atul <atulsingh1396@gmail.com>
2020-06-28 21:39:54 +0530

    changes in grid and fetching quiz api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const APIURL = "https://opentdb.com/api.php?amount=10&category=21";
+
 function App() {
-  return (
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch(APIURL)
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions(data.results);
+      });
+  }, []);
+  return questions.length != 0 ? (
     <div className="container">
       <div className="bg-teal-800 p-10 rounded-lg shadow-lg ">
-        <h2 className="text-2xl">Questions Holder</h2>
+        <h2 className="text-2xl">{questions[0].question}</h2>
       </div>
-      <div className="flex flex-wrap mt-4 justify-around">
-        <button className="bg-teal-800 w-5/12 p-4 mb-4 font-semibold">
-          Answer 1
+      <div className="grid grid-cols-2 gap-4 mt-6">
+        <button className="bg-teal-800 p-4 font-semibold rounded shadow">
+          {questions[0].correct_answer}
         </button>
-        <button className="bg-teal-800 w-5/12 p-4 font-semibold mb-4">
-          Answer 2
+        <button className="bg-teal-800 p-4 font-semibold rounded shadow">
+          {questions[0].incorrect_answers[0]}
         </button>
-        <button className="bg-teal-800 w-5/12 p-4 font-semibold">
-          Answer 3
+        <button className="bg-teal-800 p-4 font-semibold rounded shadow">
+          {questions[0].incorrect_answers[1]}
         </button>
-        <button className="bg-teal-800 w-5/12 p-4 font-semibold">
-          Answer 4
+        <button className="bg-teal-800 p-4 font-semibold rounded shadow">
+          {questions[0].incorrect_answers[2]}
         </button>
       </div>
     </div>
+  ) : (
+    <h1 className="text-teal-800 text-2xl">Loading...</h1>
   );
 }
 
